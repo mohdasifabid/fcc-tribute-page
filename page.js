e02f32bc93863d07bebc3a353c6a4718ddf18c66
asif <masifabid@outlook.com>
2021-08-27 17:23:35 +0530

    deploying
    almost done for the time being

---
+++ page.js
@@ -0,0 +1,13 @@
+const main = document.querySelector("#main")
+
+const title = document.querySelector("#title")
+
+const imgDiv = document.querySelector("#img-div")
+
+const image = document.querySelector("#image")
+
+const imgCaption = document.querySelector("#img-caption")
+
+const tributeInfo = document.querySelector("#tribute-info")
+
+const tributeLink = document.querySelector("#tribute-link")
